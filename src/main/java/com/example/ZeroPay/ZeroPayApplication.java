1816c24ddc026f4b8ad6eaba6f66efb673fa51ef
rgl-za <hyeonjeong2829@gmail.com>
2022-05-23 20:43:56 +0900

    검색 부트스트랩 적용

--- src/main/java/com/example/ZeroPay/ZeroPayApplication.java
+++ src/main/java/com/example/ZeroPay/ZeroPayApplication.java
@@ -2,8 +2,9 @@ package com.example.ZeroPay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ZeroPayApplication {
 
 	public static void main(String[] args) {
