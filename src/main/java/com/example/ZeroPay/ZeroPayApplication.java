84073ec997027a342a24770d10c07504c1303f82
jin0220 <37447282+jin0220@users.noreply.github.com>
2022-05-23 19:34:25 +0900

    개발 환경 설정

---
+++ src/main/java/com/example/ZeroPay/ZeroPayApplication.java
@@ -0,0 +1,13 @@
+package com.example.ZeroPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ZeroPay/ZeroPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZeroPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
